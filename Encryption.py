ccd7e220f456b340ebc87bbc51326bec880db346
theoz249 <tz66@kent.ac.uk>
2026-03-13 12:31:39 +0000

    3

--- Encryption.py
+++ Encryption.py
@@ -3,4 +3,7 @@ import string
 chars= string.ascii_letters+string.punctuation+string.digits+" "+"\t"
 chars = list(chars)
 
-
+def getKey():
+    key = chars.copy()
+    random.shuffle(key)
+    return key
---
+++ vault.py
@@ -0,0 +1,6 @@
+import Encryption
+import csv 
+
+
+with open("login.txt", "w") as f:  
+        f.write("username,password,key\n")
