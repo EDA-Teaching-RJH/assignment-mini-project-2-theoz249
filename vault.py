b3456f9e027ed2a5f2dba2e225db35d42163638d
theoz249 <tz66@kent.ac.uk>
2026-03-13 16:58:46 +0000

    8

--- vault.py
+++ vault.py
@@ -1,6 +1,6 @@
 import encryption
 import csv 
-
+import re
 
 try:
     with open("login.txt", "r") as f:
@@ -25,3 +25,26 @@ def newUser():
     password = encryption.encrypt(password,personalkey)
     with open("login.txt", "a") as f:
          f.write(f"{username},{password},{personalkey}\n")
+
+
+
+def logUser():
+    logIn = False
+    username = input(":")
+    password = input(":")
+    with open("loggin.txt", "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            stored_username,stored_password,key = row
+            if stored_username == username:
+                decrypted_password = encryption.decrypt(stored_password,key)
+                if decrypted_password == password:
+                    print("logged in")
+                    logIn = True 
+                    return logIn
+                else:
+                    print("incorrect password")
+                    return logIn
+            else:
+                print("username doesn't match")
+                return logIn
