749d251affc42acdb120728dee597c76f123ae2e
theoz249 <tz66@kent.ac.uk>
2026-03-13 12:33:15 +0000

    file kept overiting it self

--- vault.py
+++ vault.py
@@ -2,5 +2,14 @@ import Encryption
 import csv 
 
 
-with open("login.txt", "w") as f:  
-        f.write("username,password,key\n")
+try:
+    with open("login.txt", "r") as f:
+        print("File exists and is readable.")
+except FileNotFoundError:
+    print("File does not exist.")
+    with open("login.txt", "w") as f:  
+        f.write("username,password,key\n")   
+    print("File is now confirmed to exist (or was just created).")
+except IOError:
+    print("Could not access or create the file.")
+    pass
