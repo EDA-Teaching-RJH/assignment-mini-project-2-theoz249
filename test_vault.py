f8761fd15aff3fdd51d4bbd48be7d0eb13a20038
theoz249 <tz66@kent.ac.uk>
2026-03-15 15:23:26 +0000

    20

--- test_vault.py
+++ test_vault.py
@@ -2,3 +2,7 @@ import unittest
 from vault import passwordCheck
 
 class testvault(unittest.TestCase):
+    def test_weakP(self):
+        self.assertFalse(passwordCheck("helloworld"))
+    def test_strongP(self):
+        self.assertTrue(passwordCheck("He11owwor!d"))
